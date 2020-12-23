553783d7a7c319be5fb6a433c6b01e14c700a4b1
saisu <saisumanth203040@gmail.com>
2020-12-23 12:08:08 +0530

    Sel Commit

---
+++ src/selenium/Sel.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sel {
+	
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.youtube.com/");
+	}
+
+}
